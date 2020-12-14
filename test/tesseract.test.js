f5b74dd3ab25238380fec430babbe07b208e8c71
ty <towyuan@outlook.com>
2020-12-14 23:25:05 +0800

    chore: var to const

--- test/tesseract.test.js
+++ test/tesseract.test.js
@@ -1,14 +1,14 @@
 'use strict';
 
-var assert = require('chai').assert;
-var path = require('path');
-var tesseract = require('../lib/tesseract');
+const assert = require('chai').assert;
+const path = require('path');
+const tesseract = require('../lib/tesseract');
 
 
 describe('process', function () {
 
   it('should parse options to command', function () {
-    var options = {
+    const options = {
       binary: 'tesseract-ocr',
       l: 'chs',
       psm: '9',
@@ -17,12 +17,12 @@ describe('process', function () {
       '--user-words': './userwords',
       '--user-patterns': './userpatterns'
     };
-    var command = 'tesseract-ocr test.png /tmp/tesseract -l chs --psm 9  --tessdata-dir ./tessdata --user-words ./userwords --user-patterns ./userpatterns';
+    const command = 'tesseract-ocr test.png /tmp/tesseract -l chs --psm 9  --tessdata-dir ./tessdata --user-words ./userwords --user-patterns ./userpatterns';
     assert.equal(tesseract.command('test.png', options), command);
   });
 
   it('should return the string "node-tesseract"', function (done) {
-    var image = path.join(__dirname, 'fixtures', 'test.png');
+    const image = path.join(__dirname, 'fixtures', 'test.png');
     tesseract.process(image, function (err, text) {
       assert.notOk(err);
       assert.equal(text.trim(), 'node-tesseract');
